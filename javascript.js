41d9765bd082316cd6d54515c6c4051787781d75
Precious Kusi <146889458+preciouskusi17@users.noreply.github.com>
2023-12-04 09:10:28 -0500

    Edit javascript.js and add game()

--- javascript.js
+++ javascript.js
@@ -7,37 +7,43 @@ function getComputerChoice() {
     } else return 'Scissors'
         
 }
+ 
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === 'rock' && computerSelection==='Scissors'){
+let computerSelection = getComputerChoice;
+
+function playRound() {
+    let playerSelection = prompt('Option =  Rock, Scissors, Paper','choose one option').toLowerCase();
+
+    if (playerSelection === 'rock' && computerSelection()==='Scissors'){
         return 'You won! Rock beats Scissors';
 
-    } else  if (playerSelection.toLowerCase() === 'scissors' && computerSelection==='Rock'){
+    } else  if (playerSelection === 'scissors' && computerSelection()==='Rock'){
         return 'You lose! Rock beats Scissors';
 
-    } else   if (playerSelection.toLowerCase() === 'paper' && computerSelection==='Rock'){
+    } else   if (playerSelection === 'paper' && computerSelection()==='Rock'){
         return 'You won! Paper beats Rock';
 
-    } else  if (playerSelection.toLowerCase() === 'rock' && computerSelection==='Paper'){
+    } else  if (playerSelection === 'rock' && computerSelection()==='Paper'){
         return 'You lose! Paper beats Rock';
 
-    } else  if (playerSelection.toLowerCase() === 'paper' && computerSelection==='Scissors'){
+    } else  if (playerSelection === 'paper' && computerSelection()==='Scissors'){
         return 'You lose! Scissor beats Paper';
 
-    } else  if (playerSelection.toLowerCase() === 'scissors' && computerSelection==='Paper'){
+    } else  if (playerSelection === 'scissors' && computerSelection()==='Paper'){
         return 'You won! Scissor beats Paper';
 
-    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection==='Rock'){
+    } else if (playerSelection === 'rock' && computerSelection()==='Rock'){
         return 'It\'s a tie! Rock ties with Rock';
 
-    } else  if (playerSelection.toLowerCase() === 'scissors' && computerSelection==='scissors'){
+    } else  if (playerSelection=== 'scissors' && computerSelection()==='Scissors'){
         return 'It\'s a tie! Scissors ties with Scissors';
 
-    } else   if (playerSelection.toLowerCase() === 'paper' && computerSelection==='Paper'){
+    } else   if (playerSelection === 'paper' && computerSelection()==='Paper'){
         return 'It\'s a tie! Paper ties with Paper';
 
     } 
   }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+function game (){
+for (let i=1; i <= 5; ++i) console.log(playRound());
+}
